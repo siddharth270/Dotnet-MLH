c691e13edeba27d44ff01b91f7340bee5227c6ff
Siddharth Mehta <66232143+siddharth270@users.noreply.github.com>
2021-01-11 20:38:03 +0530

    Add files via upload

---
+++ MyApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello MLH! I am Siddharth Mehta");
+            Console.WriteLine("Thank you for such wonderfull challenges.");
+            Console.WriteLine(" ");
+        }
+    }
+}
